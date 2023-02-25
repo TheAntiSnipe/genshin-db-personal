552961f3e5ea22f6e6b0b150cf3b6c8b37e647fa
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2023-02-25 15:30:51 -0500

    Stability patch, ready for stable

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ import json
 
 init()
 
-
+class InputValidator:
+    def take_integer(self,text,color,fallback):
+        value = input(color+text)
+        while(not value.isdigit()):
+            value = input(color+fallback)
+        return value
 class TalentCostCalculator:
     def __init__(self):
         # talent_material_cost_json = open("talent_material_cost.json", "r")
@@ -32,15 +37,21 @@ class TalentCostCalculator:
         # * aggregate_cumulative_cost finish.
 
     # * Three methods:
-    # *    1. run_talent_material_calculator
-    #        Handles UI display and sends initial and final talent values to
-    #        determine_total_costs_and_display.
+    # *    1. aggregate_cumulative_cost
+    #        Handles dataframe aggregation and summation with
+    #        cumulative_material_dict.
     # *    2. determine_total_costs_and_display
     #        Handles method calls to aggregate_cumulative_cost and displays
     #        output UI.
-    # *    3. aggregate_cumulative_cost
-    #        Handles dataframe aggregation and summation with
-    #        cumulative_material_dict.
+    # *    3. run_talent_material_calculator
+    #        Handles UI display and sends initial and final talent values to
+    #        determine_total_costs_and_display.
+
+    def validate_level_input(self,whitelist,input_data,fallback):
+        if input_data in whitelist:
+            return int(input_data)
+        else:
+            return fallback
 
     def aggregate_cumulative_cost(self, initial_level, final_level):
         for index in range(initial_level - 1, final_level - 1):
@@ -78,19 +89,28 @@ class TalentCostCalculator:
                 )
 
     def run_talent_material_calculator(self):
-        print(Fore.GREEN + "\nMy character has current talent levels:")
-
-        aa_initial = int(input(Fore.LIGHTYELLOW_EX + "Auto-attack talent: "))
-        e_initial = int(input(Fore.LIGHTYELLOW_EX + "Elemental skill talent: "))
-        q_initial = int(input(Fore.LIGHTYELLOW_EX + "Elemental burst talent: "))
+        print(Fore.GREEN + "\nMy character has current talent levels: (press anything if 1)")
+        whitelist = [str(i) for i in range(2,11)]
+        fallback = 1
+        aa_initial = input(Fore.LIGHTYELLOW_EX + "Auto-attack talent: ")
+        aa_initial = self.validate_level_input(whitelist,aa_initial,fallback)
+        e_initial = input(Fore.LIGHTYELLOW_EX + "Elemental skill talent: ")
+        e_initial = self.validate_level_input(whitelist,e_initial,fallback)
+        q_initial = input(Fore.LIGHTYELLOW_EX + "Elemental burst talent: ")
+        q_initial = self.validate_level_input(whitelist,q_initial,fallback)
         initial_talent_values = [aa_initial, e_initial, q_initial]
 
-        print(Fore.GREEN + "\nI need talent levels:")
-        aa_final = int(input(Fore.LIGHTYELLOW_EX + "Auto-attack talent: "))
-        e_final = int(input(Fore.LIGHTYELLOW_EX + "Elemental skill talent: "))
-        q_final = int(input(Fore.LIGHTYELLOW_EX + "Elemental burst talent: "))
+        print(Fore.GREEN + "\nI need talent levels: (press anything if 10)")
+        whitelist = [str(i) for i in range(1,10)]
+        fallback = 10
+        aa_final = input(Fore.LIGHTYELLOW_EX + "Auto-attack talent: ")
+        aa_final = self.validate_level_input(whitelist,aa_final,fallback)
+        e_final = input(Fore.LIGHTYELLOW_EX + "Elemental skill talent: ")
+        e_final = self.validate_level_input(whitelist,e_final,fallback)
+        q_final = input(Fore.LIGHTYELLOW_EX + "Elemental burst talent: ")
+        q_final = self.validate_level_input(whitelist,q_final,fallback)
         final_talent_values = [aa_final, e_final, q_final]
-        print(Fore.LIGHTCYAN_EX + "\nThus, I need:")
+        print(Fore.LIGHTCYAN_EX + "\nThus, for",initial_talent_values,"to",final_talent_values,"I need:")
 
         self.determine_total_costs_and_display(
             initial_talent_values,
@@ -102,24 +122,39 @@ class BasicCalculationFunctions:
     #  This class covers functions that do simple calculations and don't pull from
     #  any of the csv files.
 
-    # * Two methods:
-    # *     1. mora_for_exp_calculator
+    # * Three methods:
+    # *     1. exp_input
+    #          Common input handler for 2 and 3.
+    # *     2. mora_for_exp_calculator
     #         Handles the second option, gives the number of mora needed
     #         for a given number of EXP books.
-    # *     2. exp_book_aggregator
+    # *     3. exp_book_aggregator
     #         Handles the conversion of various EXP book types to a standardized
     #         "hero's wit equivalent" format.
+    def exp_input(self):
+        print(Fore.GREEN + "\nI have the following EXP book quantities: ")
+        validate = InputValidator()
+        text = "Uncommon books: "
+        fallback = "Please enter an integer for uncommon books: "
+        color = Fore.GREEN
+        uncommon_exp_books = int(validate.take_integer(text,color,fallback))
+        text = "Rare books: "
+        fallback = "Please enter an integer for rare books: "
+        color = Fore.CYAN
+        rare_exp_books = int(validate.take_integer(text,color,fallback))
+        text = "Epic books: "
+        fallback = "Please enter an integer for epic books: "
+        color = Fore.MAGENTA
+        epic_exp_books = int(validate.take_integer(text,color,fallback))
+        return uncommon_exp_books,rare_exp_books,epic_exp_books
 
     def mora_for_exp_calculator(self):
-        print(Fore.GREEN + "\nI have the following EXP book quantities: ")
-        uncommon_exp_books = int(input(Fore.GREEN + "Uncommon books: "))
-        rare_exp_books = int(input(Fore.CYAN + "Rare books: "))
-        epic_exp_books = int(input(Fore.MAGENTA + "Epic books: "))
+        uncommon,rare,epic = self.exp_input()
         # * Cost per uncommon book is 200 mora.
         # * Cost per rare book is 1000 mora.
         # * Cost per epic book is 4000 mora.
         total_mora_required = (
-            200 * uncommon_exp_books + 1000 * rare_exp_books + 4000 * epic_exp_books
+            200 * uncommon + 1000 * rare + 4000 * epic
         )
         print(
             Fore.LIGHTYELLOW_EX
@@ -130,12 +165,9 @@ class BasicCalculationFunctions:
         )
 
     def exp_book_aggregator(self):
-        print(Fore.GREEN + "\nI have the following EXP book quantities: ")
-        uncommon_exp_books = int(input(Fore.BLUE + "Uncommon books: "))
-        rare_exp_books = int(input(Fore.CYAN + "Rare books: "))
-        epic_exp_books = int(input(Fore.MAGENTA + "Epic books: "))
+        uncommon,rare,epic = self.exp_input()
         epic_book_aggregate = (
-            epic_exp_books + rare_exp_books / 4 + uncommon_exp_books / 20
+            epic + rare / 4 + uncommon / 20
         )
         print(
             Fore.MAGENTA
@@ -149,7 +181,6 @@ class BasicCalculationFunctions:
 
 class LevelingCostCalculator:
     def __init__(self):
-        # ascend_cost_json = open("character_levelup_ascend_cost.json", "r")
         self.ascend_cost_dict = {
     "Mora": {
         "0": 24200,
@@ -271,11 +302,11 @@ class LevelingCostCalculator:
         )
 
     # * Two methods:
-    # *     1. level_resource_calculator
+    # *     1. find_total_cost_of_levels
+    #          Aggregates total resources needed and displays output UI.
+    # *     2. level_resource_calculator
     #         Handles UI display and sends the current and required level
     #         values to find_total_cost_of_levels
-    # *     2. find_total_cost_of_levels
-    #          Aggregates total resources needed and displays output UI.
 
     def find_total_cost_of_levels(self, current_level, required_level):
         for index in range(current_level-1, required_level):
@@ -303,30 +334,40 @@ class LevelingCostCalculator:
                     )
 
     def level_resource_calculator(self):
+        valid_starts = [str(i) for i in range(2,8)]
+        valid_ends = [str(i) for i in range(1,7)]
         print(Fore.GREEN + "\nMy character is currently at: ")
-        print(Fore.LIGHTYELLOW_EX + "1> Level 1")
-        print(Fore.LIGHTYELLOW_EX + "2> Level 20")
-        print(Fore.LIGHTYELLOW_EX + "3> Level 40")
-        print(Fore.LIGHTYELLOW_EX + "4> Level 50")
-        print(Fore.LIGHTYELLOW_EX + "5> Level 60")
-        print(Fore.LIGHTYELLOW_EX + "6> Level 70")
-        print(Fore.LIGHTYELLOW_EX + "7> Level 80")
+        print(Fore.LIGHTYELLOW_EX + "Any key > Level 1")
+        print(Fore.LIGHTYELLOW_EX + "2 > Level 20")
+        print(Fore.LIGHTYELLOW_EX + "3 > Level 40")
+        print(Fore.LIGHTYELLOW_EX + "4 > Level 50")
+        print(Fore.LIGHTYELLOW_EX + "5 > Level 60")
+        print(Fore.LIGHTYELLOW_EX + "6 > Level 70")
+        print(Fore.LIGHTYELLOW_EX + "7 > Level 80")
         print(
             Fore.LIGHTYELLOW_EX
             + """\n(We are assuming your character hasn't been ascended at that level. 
 If you select 20, your character should be 20/20, not 20/40.)\n"""
         )
-        current_level = int(input(Fore.GREEN + "Enter the corresponding number: "))
+        current_level = input(Fore.GREEN + "Enter the corresponding number: ")
+        if current_level not in valid_starts:
+            current_level = 1
+        else:
+            current_level = int(current_level)
 
         print(Fore.GREEN + "I want my character to be: ")
-        print(Fore.LIGHTYELLOW_EX + "1> Level 20")
-        print(Fore.LIGHTYELLOW_EX + "2> Level 40")
-        print(Fore.LIGHTYELLOW_EX + "3> Level 50")
-        print(Fore.LIGHTYELLOW_EX + "4> Level 60")
-        print(Fore.LIGHTYELLOW_EX + "5> Level 70")
-        print(Fore.LIGHTYELLOW_EX + "6> Level 80")
-        print(Fore.LIGHTYELLOW_EX + "7> Level 90")
-        required_level = int(input(Fore.GREEN + "\nEnter the corresponding number: "))
+        print(Fore.LIGHTYELLOW_EX + "1 > Level 20")
+        print(Fore.LIGHTYELLOW_EX + "2 > Level 40")
+        print(Fore.LIGHTYELLOW_EX + "3 > Level 50")
+        print(Fore.LIGHTYELLOW_EX + "4 > Level 60")
+        print(Fore.LIGHTYELLOW_EX + "5 > Level 70")
+        print(Fore.LIGHTYELLOW_EX + "6 > Level 80")
+        print(Fore.LIGHTYELLOW_EX + "Any key > Level 90")
+        required_level = input(Fore.GREEN + "\nEnter the corresponding number: ")
+        if required_level not in valid_ends:
+            required_level = 7
+        else:
+            required_level = int(required_level)
         if required_level < current_level:
             print(
                 Fore.RED
@@ -348,37 +389,31 @@ If you select 20, your character should be 20/20, not 20/40.)\n"""
 
 class MainMenu:
     def display_option_list(self):
-        option = int(
-            input(
+        option = input(
                 Fore.LIGHTBLUE_EX
                 + """\nEnter what you want to do with the database:
 1. Figure out talent level resource requirements.
 2. Figure out how much mora you need for some EXP quantity.
 3. Figure out character level + ascension resource requirements.
 4. Convert uncommon, rare and epic EXP books' EXP values to values relative to epic EXP books. 
-5. Exit.
+
+Press any other key to exit.
 
 Enter the corresponding number: """
             )
-        )
         calculation_functions = BasicCalculationFunctions()
         talent_cost_calculator = TalentCostCalculator()
         leveling_cost_calculator = LevelingCostCalculator()
-        if option == 1:
+        if option == '1':
             talent_cost_calculator.run_talent_material_calculator()
-        elif option == 2:
+        elif option == '2':
             calculation_functions.mora_for_exp_calculator()
-        elif option == 3:
+        elif option == '3':
             leveling_cost_calculator.level_resource_calculator()
-        elif option == 4:
+        elif option == '4':
             calculation_functions.exp_book_aggregator()
-        elif option == 5:
-            exit(Fore.LIGHTGREEN_EX + "Thank you for using GI-DB!")
         else:
-            print(
-                "Invalid option, please select an option between 1 and 5. You selected",
-                option,
-            )
+            exit(Fore.LIGHTGREEN_EX + "Thank you for using GI-DB!")
 
 def main():
     newMenu = MainMenu()
